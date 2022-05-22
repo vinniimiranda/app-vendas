abdc492d5d8505fbb3d9d534831467fb858ac12f
Flavio Vinicius Miranda <vinniimiranda@gmail.com>
2022-05-22 16:33:49 -0300

    chore: splash screen and rubik font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,61 @@
+import {
+  Rubik_300Light_Italic,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+} from '@expo-google-fonts/rubik';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { extendTheme, NativeBaseProvider } from 'native-base';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { SignIn } from './screens/signin';
 
+SplashScreen.preventAutoHideAsync()
+  .then((result) => console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`))
+  .catch(console.warn); // it
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          Rubik_300Light_Italic,
+          Rubik_400Regular,
+          Rubik_500Medium,
+          Rubik_600SemiBold,
+          Rubik_700Bold,
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   const theme = extendTheme({
     colors: {
       primary: {
@@ -33,11 +87,39 @@ export default function App() {
         },
       },
     },
+    fontConfig: {
+      Rubik: {
+        300: {
+          normal: 'Rubik_300Light_Italic',
+        },
+        400: {
+          normal: 'Rubik_400Regular',
+        },
+        500: {
+          normal: 'Rubik_500Medium',
+        },
+        600: {
+          normal: 'Rubik_600SemiBold',
+          bold: 'Rubik_600SemiBold',
+        },
+        700: {
+          normal: 'Rubik_700Bold',
+          bold: 'Rubik_700Bold',
+        },
+      },
+    },
+    fonts: {
+      heading: 'Rubik',
+      body: 'Rubik',
+      mono: 'Rubik_500Medium',
+    },
   });
 
   return (
-    <NativeBaseProvider theme={theme}>
-      <SignIn />
-    </NativeBaseProvider>
+    <View onLayout={onLayoutRootView} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <NativeBaseProvider theme={theme}>
+        <SignIn />
+      </NativeBaseProvider>
+    </View>
   );
 }
